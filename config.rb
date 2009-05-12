4f456466bc149a6fc0b7f14ed93bac5ceddc6b2e
Scott Tesoriere <scott@iMac.local>
2009-05-12 12:13:50 -0400

    basic html/css layout using compass

--- config.rb
+++ config.rb
@@ -1,9 +1,9 @@
 # Require any additional compass plugins here.
 project_type = :stand_alone
-css_dir = "css"
+css_dir = "stylesheets"
 sass_dir = "src"
 images_dir = "images"
 output_style = :compact
 # To enable relative image paths using the images_url() function:
 # http_images_path = :relative
-http_images_path = "/images"
+http_images_path = :relative
